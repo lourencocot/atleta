dfbea7a890fec1445cf4635d33fd77e7889edbb7
Lourenço Mendes <11900598@aluno.cotemig.com.br>
2020-06-22 11:39:02 -0300

    inciando isso

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace atletas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_idade = new System.Windows.Forms.TextBox();
+            this.lbl_idade = new System.Windows.Forms.Label();
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.lbl_peso = new System.Windows.Forms.Label();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_idade
+            // 
+            this.txt_idade.Location = new System.Drawing.Point(27, 280);
+            this.txt_idade.Name = "txt_idade";
+            this.txt_idade.Size = new System.Drawing.Size(100, 20);
+            this.txt_idade.TabIndex = 18;
+            // 
+            // lbl_idade
+            // 
+            this.lbl_idade.AutoSize = true;
+            this.lbl_idade.Location = new System.Drawing.Point(60, 245);
+            this.lbl_idade.Name = "lbl_idade";
+            this.lbl_idade.Size = new System.Drawing.Size(34, 13);
+            this.lbl_idade.TabIndex = 17;
+            this.lbl_idade.Text = "Idade";
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Location = new System.Drawing.Point(27, 203);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(100, 20);
+            this.txt_peso.TabIndex = 16;
+            // 
+            // lbl_peso
+            // 
+            this.lbl_peso.AutoSize = true;
+            this.lbl_peso.Location = new System.Drawing.Point(62, 168);
+            this.lbl_peso.Name = "lbl_peso";
+            this.lbl_peso.Size = new System.Drawing.Size(31, 13);
+            this.lbl_peso.TabIndex = 15;
+            this.lbl_peso.Text = "Peso";
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Location = new System.Drawing.Point(27, 126);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(100, 20);
+            this.txt_altura.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Altura";
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(27, 49);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Location = new System.Drawing.Point(27, 325);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(100, 25);
+            this.btn_calc.TabIndex = 19;
+            this.btn_calc.Text = "Calcular IMC";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(150, 450);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.txt_idade);
+            this.Controls.Add(this.lbl_idade);
+            this.Controls.Add(this.txt_peso);
+            this.Controls.Add(this.lbl_peso);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_idade;
+        private System.Windows.Forms.Label lbl_idade;
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.Label lbl_peso;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_calc;
+    }
+}
+
